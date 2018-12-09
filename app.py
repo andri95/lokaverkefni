04b30cf5bc231506b28361bdb9f7da89bb50ba25
andri95 <afannar@gmail.com>
2018-12-09 05:08:24 +0000

    adsfaf

--- app.py
+++ app.py
@@ -181,7 +181,8 @@ def breyta_go():
 
         with connection.cursor() as cursor:
             sql = "UPDATE `frett` SET `fyrirsogn` = %s, `innihald` = %s  WHERE `nr_frettar`=%s"
-            cursor.execute(sql, (nr_frettar), (ny_frett, nyr_titill))
+            gildi = (nr_frettar, (ny_frett, nyr_titill))
+            cursor.execute(sql, gildi)
             connection.commit()
 
     finally:
